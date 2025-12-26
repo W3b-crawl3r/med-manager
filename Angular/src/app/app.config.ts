a689b51b83fbaec55f0bb746f66299ff6e54192b
W3b-crawl3r <yy0644525147@gmail.com>
2025-12-26 09:37:14 +0100

    end of date state

--- Angular/src/app/app.config.ts
+++ Angular/src/app/app.config.ts
@@ -1,12 +1,16 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { tokenInjectorInterceptorInterceptor } from './interceptors/token-injector-interceptor-interceptor';
+import { serverLogOutInterceptorInterceptor } from './interceptors/server-log-out-interceptor-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([tokenInjectorInterceptorInterceptor, serverLogOutInterceptorInterceptor])),
   ]
 };
--- Angular/src/app/app.routes.ts
+++ Angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login-component/login-component';
+import { Dummy } from './components/dummy/dummy';
+import { authenticatedGuard } from './guards/authenticated-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: Dummy, canActivate: [authenticatedGuard] },
+];
